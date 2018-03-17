f5ec41d4f76493184246b44c52f645ae2930c2a2
Ramón Guardia López <ramon.guardialopez@gmail.com>
2018-03-17 16:42:01 +0100

    v 0.8
    initial Release

---
+++ php/sendMail.php
@@ -0,0 +1,30 @@
+<?php
+if(isset($_POST['submit'])){
+  echo "entra";
+    $to = "ramon.guardialopez@gmail.com"; // this is your Email address
+    $from = $_POST['mail']; // this is the sender's Email address
+    $first_name = $_POST['name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . " wrote the following:" . "\n\n" . $_POST['comment'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['comment'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    if(mail($to,$subject,$message,$headers)){
+      echo "pasa";
+    }else{
+      echo "error";
+    }
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    //envío el mensaje, comprobando si se envió correctamente
+      if(!$mail) {
+      echo "Error al enviar el mensaje: ";
+      } else {
+      echo "Mensaje enviado!!";
+      }
+    }
+ ?>
